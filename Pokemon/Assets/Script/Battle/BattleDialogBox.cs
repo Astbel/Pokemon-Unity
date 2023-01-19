ee8603ee29151f1008648da983f846602e3e4a10
Astbel <67743361+Astbel@users.noreply.github.com>
2023-01-19 23:52:36 +0800

    新增以下項目: 1.新增介面選單選擇,新增以下Fight moves 2.選單選擇提示玩家選擇顏色 3.控制panel定義為一個陣列而左右只會增加一,上下則會增加或減少2 4.新增戰鬥系統狀態

---
+++ Pokemon/Assets/Script/Battle/BattleDialogBox.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDialogBox : MonoBehaviour
+{
+    [SerializeField] int letterPerSecond;
+    [SerializeField] Color highlightedColor; //選單被選取時辨識的顏色
+    [SerializeField] Text dialogText;
+    /*選耽物件*/
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+    /*動作以及招式*/
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> moveTexts;
+    /*屬性以及次數*/
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
+
+    public void SetDialog(string dialog)
+    {
+        dialogText.text = dialog;
+    }
+
+    public IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach (var letter in dialog.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / letterPerSecond);
+        }
+    }
+    /*智能選單介面*/
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+
+    /*偵測選單選擇的項目,如果有選擇則顯示選項顏色否則皆為黑色*/
+    public void UpdateActionSelection(int selectedAction)
+    {
+        for (int i = 0; i < actionTexts.Count; ++i)
+        {
+            if (i == selectedAction)
+                actionTexts[i].color = highlightedColor;
+            else
+                actionTexts[i].color = Color.black;
+        }
+    }
+
+    /*技能選擇選單顏色提示*/
+    public void UpdateMoveSelection(int selectedMove,Move move)
+    {
+        for (int i = 0; i < moveTexts.Count; ++i)
+        {
+            if (i == selectedMove)
+                moveTexts[i].color = highlightedColor;
+            else
+                moveTexts[i].color = Color.black;
+        }
+         /*顯示技能PP / Type*/
+        ppText.text = $"PP {move.PP}/{move.Base.PP} ";
+        typeText.text = move.Base.Type.ToString();
+    }
+
+    /*Pokemon 技能顯示如果檢測技能是否少於4個如果比較少則顯示"-"*/
+    public void SetMoveNames(List<Move> moves)
+    {
+        for (int i = 0; i < moveTexts.Count; i++)
+        {
+            if (i < moves.Count)
+                moveTexts[i].text = moves[i].Base.Name;
+            else
+                moveTexts[i].text = "-";
+        }
+    }
+
+
+
+}
--- Pokemon/Assets/Script/Battle/BattleSystem.cs
+++ Pokemon/Assets/Script/Battle/BattleSystem.cs
@@ -2,29 +2,137 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState { Start, PlayAction, PlayerMove, Ene, yMove, Busy }
 public class BattleSystem : MonoBehaviour
 {
     [SerializeField] BattleUnit playerUnit;
     [SerializeField] BattleHud playerHud;
     [SerializeField] BattleUnit enemyUnit;
     [SerializeField] BattleHud enemyHud;
+    [SerializeField] BattleDialogBox dialogBox;
 
+    BattleState state;  //回合制狀態機
+    int currentAction; //選單變數偵測
+    int currentMove; //技能選單變數偵測
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
     /// </summary>
     private void Start()
     {
-        SetUpBattle();
+        StartCoroutine(SetUpBattle());
     }
-
-    public void SetUpBattle()
+    /*
+    修正為 IEnumerator經過顯示遇敵資訊後,再載入選單界面窗
+    */
+    public IEnumerator SetUpBattle()
     {
         playerUnit.Setup();
         playerHud.SetData(playerUnit.Pokemon);
 
         enemyUnit.Setup();
         enemyHud.SetData(enemyUnit.Pokemon);
+
+        dialogBox.SetMoveNames(playerUnit.Pokemon.Moves);
+
+        yield return dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.Name}  appeared.");
+
+        yield return new WaitForSeconds(1f); //wait fot 1sec
+
+        PlayAction(); /*初始狀態*/
+
+    }
+    /*玩家狀態 回合制設定*/
+    void PlayAction()
+    {
+        state = BattleState.PlayAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(true);
+    }
+    /*選技能階段*/
+    void PlayerMove()
+    {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void Update()
+    {
+        if (state == BattleState.PlayAction)
+        {
+            HandleActionSelection();
+        }
+        else if (state == BattleState.PlayerMove)
+        {
+            HandleMoveSelection();
+        }
+    }
+    /*Player 選擇動作*/
+    void HandleActionSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentAction < 1)
+                ++currentAction;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentAction > 0)
+                --currentAction;
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+                //Fight
+                PlayerMove();
+            }
+            else if (currentAction == 1)
+            {
+                //run
+            }
+        }
+
+    }
+    /*技能選單有一個可能是pokemon的技能萬一不足四個
+    技能在陣列中的排序定義如下
+    0 1
+    2 3
+    所以腳色在左和右時只會對陣列增加1
+    當為上下移動時陣列只會增加為2
+    */
+    void HandleMoveSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentMove < playerUnit.Pokemon.Moves.Count - 1)
+                ++currentMove;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentMove > 0)
+                --currentMove;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentMove < playerUnit.Pokemon.Moves.Count - 2)
+                currentMove += 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentMove > 1)
+                currentMove -= 2;
+        }
+        //Debug.Log(currentMove);
+        dialogBox.UpdateMoveSelection(currentMove, playerUnit.Pokemon.Moves[currentMove]);
     }
 
 
